3bb57a4ec9f43dd5ba2246e96c9b93f59cba6fe4
Andy Wang <weat0212@gmail.com>
2021-04-06 15:18:01 +0800

    Vendor Controller with test

---
+++ src/test/java/com/example/springframework/controllers/v1/VendorControllerTest.java
@@ -0,0 +1,173 @@
+package com.example.springframework.controllers.v1;
+
+import com.example.springframework.api.v1.model.VendorDTO;
+import com.example.springframework.api.v1.model.VendorListDTO;
+import com.example.springframework.controllers.RestResponseEntityExceptionHandler;
+import com.example.springframework.services.ResourceNotFoundException;
+import com.example.springframework.services.VendorService;
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.Optional;
+
+import static com.example.springframework.controllers.v1.AbstractRestControllerTest.asJsonString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(controllers = {VendorController.class})
+class VendorControllerTest {
+
+    @MockBean   //provided by Spring Context
+    VendorService vendorService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    VendorDTO vendorDTO_1;
+    VendorDTO vendorDTO_2;
+
+    @BeforeEach
+    void setUp() {
+        vendorDTO_1 = new VendorDTO("Vendor 1", VendorController.BASE_URL + "/1");
+        vendorDTO_2 = new VendorDTO("Vendor 2", VendorController.BASE_URL + "/2");
+    }
+
+    @Test
+    void getListOfVendors() throws Exception {
+
+        //given
+        VendorListDTO vendorListDTO = new VendorListDTO(Arrays.asList(vendorDTO_1, vendorDTO_2));
+
+        given(vendorService.getAllVendors()).willReturn(vendorListDTO);
+
+        mockMvc.perform(get(VendorController.BASE_URL)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.vendors", hasSize(2)));
+    }
+
+    @Test
+    void testGetVendorById() throws Exception {
+
+        //given
+        VendorDTO vendor = new VendorDTO();
+        vendor.setName("CCUMIS");
+        vendor.setVendorUrl(VendorController.BASE_URL + "/1");
+
+        when(vendorService.getVendorById(anyLong())).thenReturn(vendor);
+
+        //when
+        mockMvc.perform(get(VendorController.BASE_URL + "/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo("CCUMIS")));
+    }
+
+    @Test
+    void createNewVendor() throws Exception {
+
+        //given
+        VendorDTO vendorDTO = new VendorDTO();
+        vendorDTO.setName("CCUMIS");
+        vendorDTO.setVendorUrl(VendorController.BASE_URL + "/1");
+
+        VendorDTO returnDTO = new VendorDTO();
+        returnDTO.setName(vendorDTO.getName());
+        returnDTO.setVendorUrl(vendorDTO.getVendorUrl());
+
+        when(vendorService.createNewVendor(vendorDTO)).thenReturn(returnDTO);
+
+        //when then
+        mockMvc.perform(post(VendorController.BASE_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(vendorDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name", equalTo("CCUMIS")))
+                .andExpect(jsonPath("$.vendor_url", equalTo(VendorController.BASE_URL + "/1")));
+    }
+
+    @Test
+    void testUpdateVendor() throws Exception {
+
+        //given
+        VendorDTO vendorDTO = new VendorDTO();
+        vendorDTO.setName("CCUMIS");
+
+        VendorDTO returnDTO = new VendorDTO();
+        returnDTO.setName(vendorDTO.getName());
+        returnDTO.setVendorUrl(VendorController.BASE_URL + "/1");
+
+        when(vendorService.saveVendorByDTO(anyLong(), any(VendorDTO.class))).thenReturn(returnDTO);
+
+        //when then
+        mockMvc.perform(put(VendorController.BASE_URL + "/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(vendorDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo("CCUMIS")))
+                .andExpect(jsonPath("$.vendor_url", equalTo(VendorController.BASE_URL + "/1")));
+    }
+
+    @Test
+    void testPatchVendor() throws Exception {
+
+        //given
+        VendorDTO vendorDTO = new VendorDTO();
+        vendorDTO.setName("CCUMIS");
+
+        VendorDTO returnDTO = new VendorDTO();
+        returnDTO.setName(vendorDTO.getName());
+        returnDTO.setVendorUrl(VendorController.BASE_URL + "/1");
+
+        when(vendorService.patchVendor(anyLong(), any(VendorDTO.class))).thenReturn(returnDTO);
+
+        mockMvc.perform(patch(VendorController.BASE_URL + "/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(vendorDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo("CCUMIS")))
+                .andExpect(jsonPath("$.vendor_url", equalTo(VendorController.BASE_URL + "/1")));
+
+    }
+
+    @Test
+    void testDeleteVendor() throws Exception {
+
+        mockMvc.perform(delete(VendorController.BASE_URL + "/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(vendorService).deleteVendorById(anyLong());
+    }
+
+    @Test
+    @Ignore
+    void testNotFoundException() throws Exception {
+
+        /*when(vendorController.getVendorById(anyLong())).thenThrow(ResourceNotFoundException.class);
+
+        mockMvc.perform(get(VendorController.BASE_URL + "/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());*/
+    }
+}
