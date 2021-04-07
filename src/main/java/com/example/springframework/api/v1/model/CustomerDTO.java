504838d178ee80917e2994f629e706abeb7d6e4a
weat0212 <weat0212@gmail.com>
2021-04-07 14:45:23 +0800

    Customizing endpoint documentation

--- src/main/java/com/example/springframework/api/v1/model/CustomerDTO.java
+++ src/main/java/com/example/springframework/api/v1/model/CustomerDTO.java
@@ -1,6 +1,7 @@
 package com.example.springframework.api.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -11,7 +12,10 @@ import lombok.Data;
 @Data
 public class CustomerDTO {
 
+    @ApiModelProperty(value = "This is the first name", required = true)
     private String firstName;
+
+    @ApiModelProperty(value = "Last Name" ,required = true)
     private String lastName;
 
     @JsonProperty("customer_url")
--- src/main/java/com/example/springframework/controllers/v1/CustomerController.java
+++ src/main/java/com/example/springframework/controllers/v1/CustomerController.java
@@ -3,10 +3,9 @@ package com.example.springframework.controllers.v1;
 import com.example.springframework.api.v1.model.CustomerDTO;
 import com.example.springframework.api.v1.model.CustomerListDTO;
 import com.example.springframework.services.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -14,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
  * @date 2021/3/29 下午 01:41
  */
 
+@Api(description = "This is my Customer Controller")
 @RestController
 @RequestMapping(CustomerController.BASE_URL)
 public class CustomerController {
@@ -25,6 +25,7 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @ApiOperation(value = "This will get a list of customers", notes = "These are some notes about the API.")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public CustomerListDTO getListOfCustomers() {
